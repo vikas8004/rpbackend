201cdd05e453ceff03bafcf1cc7d7da38d29a7b1
vikas <vikas80046@gmail.com>
2024-05-08 02:11:40 +0530

    fixed some issues

--- src/controllers/resultFrontPage.js
+++ src/controllers/resultFrontPage.js
@@ -5,7 +5,7 @@ import ApiResponse from "../utils/ApiResponse.js";
 const frontPage = asyncHandler(async (req, res) => {
 //   console.log(req.body);
   const { year, rollno, schoolName } = req.body;
-  const foundDet = await Admission.findOne({ year, rollno, schoolName }).select("fullName fatherName motherName standard rollno -_id dob mobileNo");
+  const foundDet = await Admission.findOne({ year, rollno, schoolName }).select("fullName fatherName motherName standard rollno -_id dob mobileNo year");
   if (!foundDet) {
     res.status(404).send(new ApiResponse(404, "no record found", "failed"));
   } else {
